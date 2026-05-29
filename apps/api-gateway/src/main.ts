7973aba5bc5b5a66bc4b1d903cf4f2b2d047d708
RodrLH <u202212338@upc.edu.pe>
2026-05-28 19:27:17 -0500

    feat: initialize api-gateway application with global pipes, filters, swagger, and cors support

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
 
   const port = process.env.API_GATEWAY_PORT || 3000;
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`API Gateway is running on port: ${port}`);
 }
 bootstrap();
